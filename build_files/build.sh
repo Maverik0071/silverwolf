b34faddcc36471d35fb733c9dc813668a27f4416
Maverik0071 <Maverik0071@gmail.com>
2026-05-12 23:13:11 +0900

    updating build files

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux alacritty brave-origin-nightly distrobox dmenu dms dms-greeter dunst fastfetch flameshot fzf gh git grim grimshot hyprland hyprland-guiutils hyprland-protocols-devel hyprpaper hyprshot jetbrains-mono-fonts-all jgmenu jgmenu-gtktheme jgmenu-pmenu lm_sensors lsd mako neovim NetworkManager-tui picom quickshell rofi swww vim waypaper xkill zsh
 
 # Use a COPR Example:
 #
